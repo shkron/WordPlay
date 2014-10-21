273d560ddb1340c8078343b2dc34551c1930d637
Alexey Emelyanov <emelyanoff@gmail.com>
2014-10-21 15:51:47 -0700

    Initial Commit

---
+++ WorldPlay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WorldPlay
+//
+//  Created by Alex on 10/21/14.
+//  Copyright (c) 2014 Alexey Emelyanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WorldPlay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WorldPlay
+//
+//  Created by Alex on 10/21/14.
+//  Copyright (c) 2014 Alexey Emelyanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
